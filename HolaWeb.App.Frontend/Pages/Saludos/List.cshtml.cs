d0399dfb27b06a413d465ba37601633507f39fbe
JorgeVanegasReyes <reyesjedu@gmail.com>
2021-09-29 17:54:02 -0500

    Por fin se descargo

---
+++ HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HolaWeb.App.Frontend.Pages
+{
+    public class ListModel : PageModel
+    {
+    private string[] saludos= {"Sdrasvitia", "Bon Jorno", "Bon Jour","Hello"}; //atributo de la clase
+
+    public List<string> ListaSaludos {get;set;} //propiedad de la clase
+
+    
+        public void OnGet()
+        {
+            ListaSaludos = new List<string>();
+            ListaSaludos.AddRange(saludos);
+
+        }
+    
+    }
+}
