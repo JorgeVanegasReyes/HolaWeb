9ae9cf4984a02b01d1084c1262d5adc1b29810eb
JorgeVanegasReyes <reyesjedu@gmail.com>
2021-10-03 12:32:12 -0500

    cambio lista

--- HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
+++ HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
@@ -11,11 +11,8 @@ namespace HolaWeb.App.Frontend.Pages
 {
     public class ListModel : PageModel
     {
-<<<<<<< HEAD
         //private string[] saludos= {"Nuevo cambio en GITHUB","Sdrasvitia", "Bon Jorno", "Bon Jour","Hello"}; //atributo de la clase
-=======
     private string[] saludos= {"Nuevo cambio desde GITHUB a VS Code","Sdrasvitia", "Bon Jorno", "Bon Jour","Hello"}; //atributo de la clase
->>>>>>> 794d25bed44d3ca2f5e3015791c76ae6a02662a5
 
         //public List<string> ListaSaludos {get;set;} //propiedad de la clase
         private readonly IRepositorioSaludos repositorioSaludos;
